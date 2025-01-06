22eaa29e303b2649d5bea971efbd91a92f7aa92e
victor828 <veom104@gmail.com>
2025-01-06 07:01:47 -0800

    Inicializar el estado del usuario en el contexto de usuarios con valores predeterminados

--- src/context/useContextUsers.tsx
+++ src/context/useContextUsers.tsx
@@ -18,7 +18,13 @@ interface UsersProviderProps {
 
 export const UsersProvider = ({ children }: UsersProviderProps) => {
   const [users, setUsers] = useState<User[]>([]);
-  const [user, setUser] = useState<User | null>(null);
+  const [user, setUser] = useState<User | null>({
+    id: 0,
+    nombre: "",
+    correo: "",
+    fecha_nacimiento: new Date(),
+    password: "",
+  });
 
   return (
     <UsersContext.Provider value={{ users, user, setUsers, setUser }}>
