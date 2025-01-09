aca8c738f87a57074a37f7bd016cf7a9edfbd1e0
victor828 <veom104@gmail.com>
2025-01-09 11:32:43 -0800

    Eliminar las acciones de inicio de sesión y registro que utilizan el servicio de reCAPTCHA.

--- src/modules/auth/Login/index.js
+++
@@ -1,9 +0,0 @@
-import { recaptchaService } from "../../../service/recaptcha.service";
-
-export async function loginAction() {
-  const recaptchaData = await recaptchaService();
-  if (!recaptchaData.success) {
-    return { success: false };
-  }
-  return { success: true };
-}
